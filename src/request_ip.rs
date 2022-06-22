5f30b88057aa2092467dd74fadee71195c3af4ee
amigin <amigin@gmail.com>
2022-06-22 21:49:21 +0300

    Added get_real_ip_as_string

--- src/request_ip.rs
+++ src/request_ip.rs
@@ -10,4 +10,11 @@ impl<'s> RequestIp<'s> {
             RequestIp::Forwarded(forwarded_ips) => forwarded_ips[0],
         }
     }
+
+    pub fn get_real_ip_as_string(self) -> String {
+        match self {
+            RequestIp::Result(ip) => ip,
+            RequestIp::Forwarded(forwarded_ips) => forwarded_ips[0].to_string(),
+        }
+    }
 }
