f6dc22db0b3a6be50bc4306831b82aad8037fe9f
amigin <amigin@gmail.com>
2022-08-06 14:55:28 +0300

    Small adjustments

--- src/static_files_middleware/static_files.rs
+++ src/static_files_middleware/static_files.rs
@@ -10,6 +10,22 @@ pub struct StaticFilesMiddleware {
 
 impl StaticFilesMiddleware {
     pub fn new(file_folder: Option<&str>, index_files: Option<Vec<String>>) -> Self {
+        let index_files = if let Some(index_file_to_check) = index_files {
+            let mut index_files_result = Vec::with_capacity(index_file_to_check.len());
+
+            for index_file in index_file_to_check {
+                if index_file.starts_with('/') {
+                    index_files_result.push(index_file);
+                } else {
+                    index_files_result.push(format!("/{}", index_file));
+                }
+            }
+
+            Some(index_files_result)
+        } else {
+            None
+        };
+
         let file_folder = if let Some(file_folder) = file_folder {
             file_folder.to_lowercase()
         } else {
