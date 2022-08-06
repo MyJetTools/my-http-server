b8e0b92662f3500582508091bbecd64f6a1dbf9e
amigin <amigin@gmail.com>
2022-08-06 14:37:24 +0300

    StaticFiles now has indexFile

--- src/middlewares/static_files.rs
+++ src/middlewares/static_files.rs
@@ -5,17 +5,21 @@ use crate::{
 
 pub struct StaticFilesMiddleware {
     pub file_folder: String,
+    pub index_files: Option<Vec<String>>,
 }
 
 impl StaticFilesMiddleware {
-    pub fn new(file_folder: Option<&str>) -> Self {
+    pub fn new(file_folder: Option<&str>, index_files: Option<Vec<String>>) -> Self {
         let file_folder = if let Some(file_folder) = file_folder {
             file_folder.to_lowercase()
         } else {
             super::files::DEFAULT_FOLDER.to_string()
         };
 
-        Self { file_folder }
+        Self {
+            file_folder,
+            index_files,
+        }
     }
 }
 
@@ -26,7 +30,22 @@ impl HttpServerMiddleware for StaticFilesMiddleware {
         ctx: &mut HttpContext,
         get_next: &mut HttpServerRequestFlow,
     ) -> Result<HttpOkResult, HttpFailResult> {
-        let file = format!("{}{}", self.file_folder, ctx.request.get_path_lower_case());
+        let path = {
+            let mut path = ctx.request.get_path_lower_case();
+
+            if path == "/" {
+                if let Some(index_files) = &self.index_files {
+                    for index_file in index_files {
+                        path = index_file.as_str();
+                        break;
+                    }
+                }
+            }
+
+            path
+        };
+
+        let file = format!("{}{}", self.file_folder, path);
 
         match super::files::get(file.as_str()).await {
             Ok(file_content) => {
