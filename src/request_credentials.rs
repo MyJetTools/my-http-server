437d5a104b619698b8ba0b0c2effaf488d328432
amigin <amigin@gmail.com>
2022-11-07 15:42:07 +0200

    Upgraded claims

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -1,4 +1,6 @@
+use crate::HttpRequest;
+
 pub trait RequestCredentials {
     fn get_id(&self) -> &str;
-    fn get_claim(&self, claim_id: &str) -> Option<&str>;
+    fn get_claim(&self, request: &HttpRequest, claim_id: &str) -> Option<&str>;
 }
