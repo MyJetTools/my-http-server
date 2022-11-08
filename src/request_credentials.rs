07fc586453de765e0733be6de51556642e2aa481
amigin <amigin@gmail.com>
2022-11-08 17:24:09 +0200

    Added allowed_ip

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -6,6 +6,22 @@ pub struct RequestClaim {
     pub allowed_ips: Option<Vec<String>>,
 }
 
+impl RequestClaim {
+    pub fn is_ip_allowed(&self, ip: &str) -> bool {
+        if let Some(allowed_ips) = &self.allowed_ips {
+            for allowed_ip in allowed_ips {
+                if allowed_ip == ip {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        true
+    }
+}
+
 pub trait RequestCredentials {
     fn get_id(&self) -> &str;
     fn get_claims(&self) -> Option<&[RequestClaim]>;
