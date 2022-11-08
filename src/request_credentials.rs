78bb9cbb1afdf1ef60e5184347249a6d53e58fb9
amigin <amigin@gmail.com>
2022-11-08 22:47:42 +0200

    Debugging Claims

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -1,28 +1,12 @@
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 
-pub struct RequestClaim {
-    pub id: String,
-    pub expires: DateTimeAsMicroseconds,
-    pub allowed_ips: Option<Vec<String>>,
-}
-
-impl RequestClaim {
-    pub fn is_ip_allowed(&self, ip: &str) -> bool {
-        if let Some(allowed_ips) = &self.allowed_ips {
-            for allowed_ip in allowed_ips {
-                if allowed_ip == ip {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        true
-    }
+pub trait RequestClaim {
+    fn get_id(&self) -> &str;
+    fn get_expires(&self) -> DateTimeAsMicroseconds;
+    fn get_allowed_ips(&self) -> Option<&Vec<String>>;
 }
 
 pub trait RequestCredentials {
     fn get_id(&self) -> &str;
-    fn get_claims(&self) -> Option<&[RequestClaim]>;
+    fn get_claims<'s>(&'s self) -> Option<&[&'s dyn RequestClaim]>;
 }
