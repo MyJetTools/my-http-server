c6a269cf94e96f1038e50265fd9cf5ef4516ee3c
amigin <amigin@gmail.com>
2022-11-08 13:49:55 +0200

    Fixed get claims

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -1,6 +1,12 @@
-use crate::HttpRequest;
+use rust_extensions::date_time::DateTimeAsMicroseconds;
+
+pub struct RequestClaim {
+    pub id: String,
+    pub expires: DateTimeAsMicroseconds,
+    pub allowed_ips: Option<Vec<String>>,
+}
 
 pub trait RequestCredentials {
     fn get_id(&self) -> &str;
-    fn get_claim(&self, request: &HttpRequest, claim_id: &str) -> Option<&str>;
+    fn get_claims(&self) -> Option<&[RequestClaim]>;
 }
