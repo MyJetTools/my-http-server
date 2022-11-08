642215869c0423b91f8db50c3112443984ad65f6
amigin <amigin@gmail.com>
2022-11-08 23:06:59 +0200

    Adding

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -10,3 +10,19 @@ pub trait RequestCredentials {
     fn get_id(&self) -> &str;
     fn get_claims<'s>(&'s self) -> Option<RequestClaim<'s>>;
 }
+
+impl<'s> RequestClaim<'s> {
+    pub fn is_ip_allowed(&self, ip: &str) -> bool {
+        if let Some(allowed_ips) = &self.allowed_ips {
+            for allowed_ip in allowed_ips {
+                if allowed_ip == ip {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        true
+    }
+}
