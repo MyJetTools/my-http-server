c054a40d96e5e6afd9d977fc4c9a95675c654c7b
amigin <amigin@gmail.com>
2022-11-08 22:56:13 +0200

    Adjusting Claims

--- src/request_credentials.rs
+++ src/request_credentials.rs
@@ -1,14 +1,12 @@
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 
-pub trait RequestClaim {
-    fn get_id(&self) -> &str;
-    fn get_expires(&self) -> DateTimeAsMicroseconds;
-    fn get_allowed_ips(&self) -> Option<&Vec<String>>;
+pub struct RequestClaim<'s> {
+    pub id: &'s str,
+    pub expires: DateTimeAsMicroseconds,
+    pub allowed_ips: &'s Option<Vec<String>>,
 }
 
 pub trait RequestCredentials {
     fn get_id(&self) -> &str;
-    fn get_claims<'s, TIterator: Iterator<Item = &'s dyn RequestClaim>>(
-        &'s self,
-    ) -> Option<TIterator>;
+    fn get_claims<'s>(&'s self) -> Option<RequestClaim<'s>>;
 }
