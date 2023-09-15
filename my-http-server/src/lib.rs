36ba3d7b28384a4a8b9c74a4d23c7ada94b4e5bd
amigin <amigin@gmail.com>
2023-09-15 18:23:20 +0300

    Namespace

--- my-http-server/src/lib.rs
+++ my-http-server/src/lib.rs
@@ -10,4 +10,4 @@ pub extern crate my_http_server_web_sockets as web_sockets;
 #[cfg(feature = "signalr")]
 pub extern crate my_http_server_signalr_middleware as signalr;
 
-pub extern crate my_http_server_core as core;
+pub use my_http_server_core::*;
