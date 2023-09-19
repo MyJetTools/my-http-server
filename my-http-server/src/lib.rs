b5817249b8b7a7971ec29a69ab4af1dcc2713964
amigin <amigin@gmail.com>
2023-09-19 15:47:13 +0300

    Fixes with StaticFiles Middleware export

--- my-http-server/src/lib.rs
+++ my-http-server/src/lib.rs
@@ -11,6 +11,6 @@ pub extern crate my_http_server_web_sockets as web_sockets;
 pub extern crate my_http_server_signalr_middleware as signalr;
 
 #[cfg(feature = "static-files")]
-pub extern crate static_files_middleware;
+pub use static_files_middleware::*;
 
 pub use my_http_server_core::*;
