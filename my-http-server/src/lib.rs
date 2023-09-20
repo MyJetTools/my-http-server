ca259f3146a761ef04321b5afc05fff1d2299dce
RsSvc <wonderhays@gmail.com>
2023-09-20 21:57:12 +0300

    added export of signalr_r macros

--- my-http-server/src/lib.rs
+++ my-http-server/src/lib.rs
@@ -4,11 +4,16 @@ pub extern crate my_http_server_swagger as macros;
 #[cfg(any(feature = "macros", feature = "controllers"))]
 pub extern crate my_http_server_controllers as controllers;
 
-#[cfg(any(feature = "web-sockets", feature = "signal-r"))]
+#[cfg(any(feature = "websocket"))]
 pub extern crate my_http_server_web_sockets as web_sockets;
 
 #[cfg(feature = "signal-r")]
-pub extern crate my_http_server_signal_r_middleware as signal_r;
+pub mod signal_r{
+    extern crate my_http_server_signal_r_middleware;
+    pub use my_http_server_signal_r_middleware::*;
+    pub extern crate signal_r_macros as macros;
+}
+
 
 #[cfg(feature = "static-files")]
 pub use static_files_middleware::*;
