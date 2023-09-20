f3d26b84ce5f8710508f9484537198cdc9160b31
amigin <amigin@gmail.com>
2023-09-20 20:32:16 +0300

    Fixed SignalR namespaces

--- my-http-server/src/lib.rs
+++ my-http-server/src/lib.rs
@@ -4,11 +4,11 @@ pub extern crate my_http_server_swagger as macros;
 #[cfg(any(feature = "macros", feature = "controllers"))]
 pub extern crate my_http_server_controllers as controllers;
 
-#[cfg(any(feature = "web-sockets", feature = "signalr"))]
+#[cfg(any(feature = "web-sockets", feature = "signal-r"))]
 pub extern crate my_http_server_web_sockets as web_sockets;
 
-#[cfg(feature = "signalr")]
-pub extern crate my_http_server_signalr_middleware as signalr;
+#[cfg(feature = "signal-r")]
+pub extern crate my_http_server_signal_r_middleware as signal_r;
 
 #[cfg(feature = "static-files")]
 pub use static_files_middleware::*;
