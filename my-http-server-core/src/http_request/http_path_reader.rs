b529f4a7b60076f1cc70a15a83dfd7809167a800
amigin <amigin@gmail.com>
2024-06-05 19:04:54 +0200

    Small Improvements

--- my-http-server-core/src/http_request/http_path_reader.rs
+++ my-http-server-core/src/http_request/http_path_reader.rs
@@ -16,6 +16,34 @@ impl<'s> HttpPathReader<'s> {
         self.data.to_string()
     }
 
+    pub fn equals_to(&self, path: &str) -> bool {
+        self.data == path
+    }
+
+    pub fn equals_to_case_insensitive(&self, path: &str) -> bool {
+        if path.len() != self.data.len() {
+            return false;
+        }
+
+        let mut self_chars = self.data.chars();
+
+        for path_char in path.chars() {
+            let next_char = self_chars.next();
+
+            if next_char.is_none() {
+                return false;
+            }
+
+            let next_char = next_char.unwrap();
+
+            if next_char.to_ascii_lowercase() != path_char.to_ascii_lowercase() {
+                return false;
+            }
+        }
+
+        true
+    }
+
     pub fn to_short_string_or_string(&self) -> MaybeShortString {
         let as_bytes = self.data.as_bytes();
 
@@ -83,5 +111,19 @@ mod tests {
 
         assert_eq!(false, reader.starts_with_case_insensitive("/mypath"));
         assert_eq!(false, reader.starts_with_case_insensitive("/MyPath"));
+    }
+
+    #[test]
+    fn test_equals_to_case_insensitive() {
+        let path = "/MyPath";
+
+        let reader = HttpPathReader::new(path);
+
+        assert_eq!(true, reader.equals_to_case_insensitive("/mypath"));
+        assert_eq!(true, reader.equals_to_case_insensitive("/MyPath"));
+
+        assert_eq!(false, reader.equals_to_case_insensitive("/mypat"));
+        assert_eq!(false, reader.equals_to_case_insensitive("/MyPatht"));
+        assert_eq!(false, reader.equals_to_case_insensitive("/mypatc"));
     }
 }
