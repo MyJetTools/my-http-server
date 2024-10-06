4e745189c5f3a8feaa9cf05856e759c9c48ef6e7
amigin <amigin@gmail.com>
2024-10-06 18:15:25 +0300

    Exposed WsMessage

--- my-http-server-web-sockets/src/lib.rs
+++ my-http-server-web-sockets/src/lib.rs
@@ -5,3 +5,5 @@ pub use helpers::handle_web_socket_upgrade;
 pub use my_web_socket::*;
 pub use my_web_socket_callback::*;
 mod web_sockets_upgrade;
+
+pub type WsMessage = hyper_tungstenite::tungstenite::Message;
