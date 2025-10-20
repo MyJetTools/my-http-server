67623f7516437d92ac38b728af9f05e0b8cb8ba3
amigin <amigin@gmail.com>
2025-10-20 12:27:58 +0300

    Added stream support

--- my-http-server-core/src/http_output/http_output_as_stream.rs
+++ my-http-server-core/src/http_output/http_output_as_stream.rs
@@ -1,17 +1,34 @@
 use bytes::Bytes;
 use http_body_util::StreamBody;
+use rust_extensions::StrOrString;
 
 use crate::{HttpFailResult, HttpOkResult};
 
 pub struct HttpOutputAsStream {
     rx: futures::channel::mpsc::Receiver<Result<hyper::body::Frame<Bytes>, hyper::Error>>,
+    headers: Vec<(String, String)>,
 }
 
 impl HttpOutputAsStream {
     pub fn new(
         rx: futures::channel::mpsc::Receiver<Result<hyper::body::Frame<Bytes>, hyper::Error>>,
     ) -> Self {
-        Self { rx }
+        Self {
+            rx,
+            headers: Vec::new(),
+        }
+    }
+
+    pub fn with_header<'k, 'v>(
+        mut self,
+        key: impl Into<StrOrString<'k>>,
+        value: impl Into<StrOrString<'v>>,
+    ) -> Self {
+        let key = key.into();
+        let value = value.into();
+
+        self.headers.push((key.to_string(), value.to_string()));
+        self
     }
     pub fn get_result(self) -> Result<HttpOkResult, HttpFailResult> {
         use http_body_util::BodyExt;
@@ -22,7 +39,13 @@ impl HttpOutputAsStream {
         let stream_body = StreamBody::new(rx);
         let boxed_body = stream_body.map_err(|e: hyper::Error| e.to_string()).boxed();
 
-        let response = hyper::Response::builder().body(boxed_body).unwrap();
+        let mut builder = hyper::Response::builder();
+
+        for header in self.headers {
+            builder = builder.header(header.0, header.1);
+        }
+
+        let response = builder.body(boxed_body).unwrap();
 
         Ok(HttpOkResult {
             write_telemetry: false,
