a191893760a3b4359339618c028d01eba7e063a3
amigin <amigin@gmail.com>
2026-07-14 01:46:11 +0300

    Remove my-http-utils module and its components

--- my-http-utils/src/http_path/mod.rs
+++
@@ -1,5 +0,0 @@
-mod path_segment;
-mod path_segments;
-
-pub use path_segment::PathSegment;
-pub use path_segments::{GetPathValueResult, PathSegments};
--- my-http-utils/src/url_decoder/normal_state.rs
+++
@@ -1,9 +0,0 @@
-pub struct NormalState {}
-
-const SWITCH_SYMBOL: u8 = '%' as u8;
-
-impl NormalState {
-    pub fn get_next(&self, next: u8) -> bool {
-        return next != SWITCH_SYMBOL;
-    }
-}
